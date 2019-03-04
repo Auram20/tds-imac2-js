e3e204cc4b64332e29234e8fc4a517393d833f16
Auram20 <myriamanik97@gmail.com>
2019-03-04 10:56:15 +0100

    Test forked

--- src/td01/td01.js
+++ src/td01/td01.js
@@ -5,14 +5,23 @@ import hens from './hens.js'
  * Convertis le code ci-dessous en ES6, qu'importe la méthode
 */
 
-var sum = 0
+/*var sum = 0
 var numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 for (var i = 0; i < numbers.length; ++i) {
   sum += numbers[i]
 }
 
-export var ex1 = sum
+export var ex1 = sum*/
+
+let sum=0 
+const numbers=[0, 1, 1, 2, 3, 5, 8, 13, 21]
+for (let i = 0; i < numbers.length; ++i) 
+  {
+    sum += numbers[i]
+  }
+
+export const ex1 = sum
 
 /**
  * Exercice 2
@@ -20,17 +29,18 @@ export var ex1 = sum
  * En même temps, convertis ce code en ES6
  */
 
-var animals1 = ['dog', 'cat', 'faverjon', 'bird']
-var animals2 = ['lion', 'squirrel', 'bear', 'pig']
+const animals1 = ['dog', 'cat', 'faverjon', 'bird']
+const animals2 = ['lion', 'squirrel', 'bear', 'pig']
 
-export var allTheAnimals = [] // TODO
-export var ex2 = allTheAnimals
+export let allTheAnimals = animals1.concat(animals2) // TODO
+export const ex2 = allTheAnimals
 
 /**
  * Exercice 3
  * Convertis ce code en ES6 en utilisant les `fat arrow functions`
  */
 
+/** 
 function makeDogACat (thing) {
   if (thing === 'dog') {
     return 'cat'
@@ -39,13 +49,20 @@ function makeDogACat (thing) {
   return thing
 }
 
-export var ex3 = makeDogACat
+export var ex3 = makeDogACat*/
+
+
+export const ex3 = (thing) => 
+{
+  return (thing==='dog') ? 'cat' : thing 
+}
 
 /**
  * Exercice 4
  * Que retourne cette fonction ?
  */
 
+// La fonction renvoie cat 
 export function scope () {
   const a = 'dog'
   if (a === 'dog') {
@@ -66,9 +83,11 @@ export function scope () {
  */
 
 const number = 41
-export const transformNumber = () => {} // TODO
+export const  makeSomethingOutOfNumber= (f,num) => {return f(num)} // TODO
 
-export const makeSomethingOutOfNumber = null // TODO
+export const  transformNumber= (n) => {return n + 1}// TODO  
+
+transformNumber(transformNumber,number)
 
 /**
  * Exercice 6
@@ -78,25 +97,25 @@ export const makeSomethingOutOfNumber = null // TODO
  * et le nom du rédacteur afin de générer l'article signé.
  */
 
-export const articleBody = `Le pari de ce projet est de se centrer sur les images du quotidien de l’école et de s’intéresser à la construction
+export const articlebody = `Le pari de ce projet est de se centrer sur les images du quotidien de l’école et de s’intéresser à la construction
 de l’individu à la lumière des transformations numériques dans l’éducation. Il s’agit de travailler l’espace du hall
 d’accueil du nouveau rectorat pour mettre en résonance l’expérience de ses usagers avec une forme visuelle apprenante,
 générative, et qui réfléchit les expériences d’apprentissage d’aujourd’hui.
 Doué de facultés d’apprentissage automatique, le programme imaginé avec des chercheurs en analyse de l'image - régit,
 répartit et génère les signes graphiques issus d'un index constitué d'observations sur le motif de l'école d'aujourd'hui.
 Selon des critères liés aux étapes d’acquisition de la connaissance, les dessins sont ainsi décomposés, analysés,
-séquencés en grille, et enfin regénérés pour prendre vie et forme dans le nouveau bâtiment.`
+séquencés en grille, et enfin regénérés pour prendre vie et forme dans le nouveau bâtiment.`;
 
 export const sig1 = (fullName) => `${fullName} l'immortel.le`
 export const sig2 = (fullName) => fullName
 export const sig3 = (fullName) => `Jean-Michel ${fullName}`
 export const sig4 = (fullName) => `${fullName}, First of Her Name, the Unburnt, Queen of the Andals and the First Men, Khaleesi of the Great Grass Sea, Breaker of Chains, and Mother of Dragons`
 
-export const generateArticleWithSignature = () => {} // TODO
-export const finalArticle = generateArticleWithSignature(/* ... */) // TODO
+export const generateArticleWithSignature = (article, signature, name) => {return article+(signature(name))} // TODO
+export const finalArticle = generateArticleWithSignature(articlebody,sig1,'machin') // TODO
 
 /**
- * Exercice 7
+ * Exercice 7   
  * La prog ça vous a saoulé du coup vous êtes partis gérer un zoo dans le Vercors, en emmenant qq ESIPE pour commencer.
  * Vous avez besoin de gérer votre cheptel, pour ça il va falloir coder un peu plus. (dézoo pas dézoo)
  *
@@ -118,8 +137,8 @@ export const genAnimal = (name, species, legs, age, furColor) => ({
 })
 
 // Crée donc l'animal de ton choix :
-export const sampleAnimal = genAnimal(/* ... */)
-
+export const sampleAnimal = genAnimal('Machin','Bidule',4,12,'Bleu')
+// console.log(sampleAnimal)
 // Ton animal souhaite changer de couleur de fourrure, comment répercutes-tu la modif sur ton object ?
 
 // 7.1
@@ -127,7 +146,8 @@ export const sampleAnimal = genAnimal(/* ... */)
 // en écrivant 'hen' (poule en anglais) à chaque fois pour l'espèce.
 // Tu ne réécriras PAS de code créant un object.
 
-export const generateHen = () => {} // TODO
+export const generateHen = (name, legs, age, furColor) => {return genAnimal(name,'hen',legs,age,furColor)} // TODO
+export const Poules =[generateHen('poule1',2,1,'Bleu'),generateHen('poule2',2,12,'Bleu'),generateHen('poule3',2,10,'Bleu'),generateHen('poule4',2,123,'Bleu'),generateHen('poule5',2,2,'Bleu'),generateHen('poule6',2,44,'Bleu'),generateHen('poule7',2,98,'Bleu'),generateHen('poule8',2,232,'Bleu')]
 
 // 7.2
 // Les poules sont arrivées, on souhaiterait pouvoir avoir un inventaire précis de l'état de nos bêtes.
@@ -136,7 +156,12 @@ export const generateHen = () => {} // TODO
 // On souhaite écrire la fonction qui prend les poules en entrée et renvoie un objet constitué du nom de la poule
 // et de son âge. On utilisera Array.map
 
-export const hensOnlyNameAndAge = (hens) => {} // TODO
+
+export const hensOnlyNameAndAge = (hens) => 
+{ 
+   return hens.map(item => {item.name, item.age})
+} 
+console.log(hensOnlyNameAndAge(Poules))
 
 // 7.3
 // On souhaite savoir si nos poules sont bien portantes et ne souffrent pas de mutations génétiques affectant leurs pattes.
